1025bf92810bccd532df44cd13570f8718b95711
Ed Charbeneau <ed.charbeneau@gmail.com>
2022-06-06 21:42:07 -0400

    Add project files.

---
+++ PresentationWeb/Program.cs
@@ -0,0 +1,12 @@
+﻿using HighlightBlazor;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using PresentationWeb;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddHighlight();
+await builder.Build().RunAsync();
